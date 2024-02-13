ced8cf03c6772916fe5df5f5f8a84aae0be02317
TIAGO GALHOTOXFX <tiagogalhoto2@hotmail.com.br>
2024-02-13 17:27:45 -0300

    Adicionar arquivos de projeto.

---
+++ Sistema.View/FrmCadUsuario.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sistema.Entidade;
+using Sistema.Model;
+
+namespace Sistema.View
+{
+    public partial class FrmCadUsuario : Form
+    {
+        UsuarioEnt ObjTabela = new UsuarioEnt(); 
+        public FrmCadUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            opc = "Novo";
+            iniciarOpc();
+        }
+        private string opc = "";
+        private void iniciarOpc()
+        {
+            switch (opc)
+            {
+                case "Novo":
+                    HabilitarCampos();
+                    LimparCampos();
+                    break;
+
+                case "Salvar":
+                    try
+                    {
+                        ObjTabela.Nome = txtNome.Text;
+                        ObjTabela.Senha = txtUsuario.Text;
+                        ObjTabela.Usuario = txtSenha.Text;
+                    }
+                    catch (Exception Ex)
+                    {
+                        MessageBox.Show("Ocorreu um erro ao salvar !... ");
+                        throw;
+                        int X = UsuarioModel.Inserir(ObjTabela);
+                    }
+                    break;
+
+                case "Excluir":
+                    break;
+                
+                case "Editar":
+                    break; 
+            }
+        }
+        private void HabilitarCampos()
+        {
+            txtNome.Enabled = true;
+            txtUsuario.Enabled = true;
+            txtSenha.Enabled = true;
+        }
+        private void LimparCampos()
+        {
+            txtNome.Text = "";
+            txtSenha.Text = "";
+            txtUsuario.Text = "";
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            opc = "Salvar";
+            iniciarOpc();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            opc = "Excluir";
+            iniciarOpc();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            opc = "Editar";
+            iniciarOpc();
+        }
+    }
+}
---
+++ Sistema.View/Login.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Sistema.View
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuário = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Usuário :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " Senha :";
+            // 
+            // txtUsuário
+            // 
+            this.txtUsuário.Location = new System.Drawing.Point(98, 29);
+            this.txtUsuário.Name = "txtUsuário";
+            this.txtUsuário.Size = new System.Drawing.Size(156, 20);
+            this.txtUsuário.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(98, 55);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(156, 20);
+            this.txtSenha.TabIndex = 3;
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Location = new System.Drawing.Point(167, 107);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(16, 13);
+            this.lblMensagem.TabIndex = 4;
+            this.lblMensagem.Text = "...";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(98, 81);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(179, 81);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 6;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 153);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuário);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmLogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsuário;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
